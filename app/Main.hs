3296f078a7d99260cdcb0495c088418f05ca8b4d
ctoagn1 <qwezese@gmail.com>
2026-07-14 22:29:57 -0400

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,7 @@ main = do
     -- TODO: Uncomment the code below to pass the first stage
     putStr "$ "
     hFlush stdout
+    cmd <- getLine
+    putStrLn $ cmd ++ ": command not found"
+    hFlush stdout
     pure ()
