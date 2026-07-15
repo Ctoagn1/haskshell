37d12fd65e9abfa0d8508f18194e3521141b563b
ctoagn1 <qwezese@gmail.com>
2026-07-15 09:48:29 -0400

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,46 @@
 module Main (main) where
 
 import System.IO (hFlush, stdout)
-import System.Exit (exitSuccess)
+
+
+data EvaluatedResult = PrintAndContinue String | Exit | Continue
 
 main :: IO ()
 main = do
-    -- TODO: Uncomment the code below to pass the first stage
     putStr "$ "
     hFlush stdout
     cmd <- getLine
-    let args = words cmd 
-    case head args of 
-        "exit" -> exitSuccess
-        "echo" -> putStrLn $ unwords $ drop 1 args 
-        _ -> putStrLn $ cmd ++ ": command not found"
+    handleEval $ eval cmd
+    
+eval :: String -> EvaluatedResult
+eval args = if null args then Continue else eval' (getCommand args) (getRemainingArgs args)
+
+eval' :: String -> String -> EvaluatedResult
+eval' command remainingArgs = case command of 
+    "exit" -> Exit
+    "echo" -> PrintAndContinue remainingArgs
+    "type" -> PrintAndContinue $ handleTypeCommand remainingArgs
+    _ -> PrintAndContinue $ command ++ ": command not found"
+
+handleTypeCommand :: String -> String
+handleTypeCommand remainingArgs = case remainingArgs of
+    x | x `elem` ["exit", "echo", "type"] -> x ++ " is a shell builtin"
+    _ -> remainingArgs <> ": not found"
+
+getCommand :: String -> String
+getCommand args = head (words args)
+
+getRemainingArgs :: String -> String
+getRemainingArgs args  = unwords (tail $ words args)
+
+handleEval :: EvaluatedResult -> IO ()
+handleEval evaluatedResult = case evaluatedResult of
+    PrintAndContinue str -> printAndContinue str
+    Continue -> main
+    Exit -> pure ()
+
+printAndContinue :: String -> IO ()
+printAndContinue str = do
+    putStrLn str
     hFlush stdout
     main
-    pure ()
